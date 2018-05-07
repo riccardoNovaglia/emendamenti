234acaefb1ce3e62be2010eee3569ac37e6aa370
novar <riccardo.novaglia@gmail.com>
2018-05-07 21:05:24 +0100

    Update 15th to remove sponsor parties if there is no sponsor and add actors definition.

--- 15.py
+++ 15.py
@@ -60,17 +60,20 @@ def update_sponsors_parties(find_and_replace, senators_filename):
 
     def update(a):
         sponsors = a['SPONSOR']
+        if sponsors == '':
+            a['SPONSOR_PARTIES'] = ''
+            return a
+        else:
+            sponsors_list = sponsors.split(';')
+            all_matching_parties = []
+            for sponsor in sponsors_list:
+                for senator in senators:
+                    if sponsor.lower() in senator.name.lower():
+                        all_matching_parties.extend(senator.parties)
 
-        sponsors_list = sponsors.split(';')
-        all_matching_parties = []
-        for sponsor in sponsors_list:
-            for senator in senators:
-                if sponsor.lower() in senator.name.lower():
-                    all_matching_parties.extend(senator.parties)
+            a['SPONSOR_PARTIES'] = ';'.join(all_matching_parties)
 
-        a['SPONSOR_PARTIES'] = ';'.join(all_matching_parties)
-
-        return a
+            return a
 
     def headers_lambda(a):
         copy = a.copy()
@@ -81,14 +84,40 @@ def update_sponsors_parties(find_and_replace, senators_filename):
         .find_and_replace_with_headers(headers_lambda=headers_lambda, update_lambda=update)
 
 
+def define_actors(find_and_replace):
+    def headers_lambda(a):
+        copy = a.copy()
+        copy['ACTORS'] = 'anything - only for header'
+        return copy.keys()
+
+    def update(a):
+        parties = a['SPONSOR_PARTIES']
+        if "L'Ulivo" in parties and "Forza Italia" in parties:
+            a["ACTORS"] = "cross-party"
+        elif "L'Ulivo" in parties:
+            a["ACTORS"] = "gov_bb"
+        elif "Forza Italia" in parties:
+            a["ACTORS"] = "opposition"
+        elif "Lega Nord" in parties:
+            a["ACTORS"] = "opposition"
+        else:
+            a["ACTORS"] = ""
+        return a
+
+    find_and_replace \
+        .find_and_replace_with_headers(headers_lambda=headers_lambda, update_lambda=update)
+
+
 if __name__ == '__main__':
     source_dataset = './data/amendments leg 15.csv'
 
-    remove_trailing_commas(FindAndReplace(source_dataset, './outputs/15.1_no_commas.csv'))
-
-    update_commissione(FindAndReplace('./outputs/15.1_no_commas.csv', './outputs/15.2_commissione.csv'))
-
-    update_sponsor(FindAndReplace('./outputs/15.2_commissione.csv', './outputs/15.3_sponsors.csv'))
-
+    # remove_trailing_commas(FindAndReplace(source_dataset, './outputs/15.1_no_commas.csv'))
+    #
+    # update_commissione(FindAndReplace('./outputs/15.1_no_commas.csv', './outputs/15.2_commissione.csv'))
+    #
+    # update_sponsor(FindAndReplace('./outputs/15.2_commissione.csv', './outputs/15.3_sponsors.csv'))
+    #
     update_sponsors_parties(FindAndReplace('./outputs/15.3_sponsors.csv', './outputs/15.final_parties.csv'),
                             senators_filename='senators_parties_legislature_15.csv')
+
+    define_actors(FindAndReplace('./outputs/15.final_parties.csv', './outputs/15.actors_coded.csv'))
