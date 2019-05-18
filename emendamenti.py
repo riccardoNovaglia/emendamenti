b3a1b7e753cddb95a956c712e0c538d0565bd023
roberta <damianiroberta@hotmail.com>
2019-05-18 16:08:26 +0100

    Deal with multiple legislatures.

--- emendamenti.py
+++ emendamenti.py
@@ -11,13 +11,13 @@ class Senator:
 
 
 def get_amendments_from_csv():
-    with open('./data/Chamber_amend_BuonaScuola.csv', newline='') as csvfile:
+    with open('./data/senato/sen_emendamenti_gov_18.csv', newline='', encoding='utf-8-sig') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         return [row for row in reader]
 
 
-def get_senators():
-    with open('./data/camera_and_parties/camera_parties_legislature_17.csv', newline='') as csvfile:
+def get_senators(legislature_number):
+    with open('./data/senato/leg_{}.csv'.format(legislature_number), newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         return [Senator(row['NAME'], row['PARTIES'].split(',')) for row in reader]
 
@@ -42,27 +42,41 @@ def names_match(sponsor_name, senator_name):
     return True
 
 
+def find_matching_parties(sponsors, legislature_senators):
+    all_matching_parties = []
+    for sponsor in sponsors:
+        try:
+            matching_parties = next(
+                senator.parties for senator in legislature_senators if names_match(sponsor, senator.name))
+            all_matching_parties.extend(matching_parties)
+        except StopIteration as stop:
+            not_found.add(sponsor)
+    return all_matching_parties
+
+
 if __name__ == '__main__':
-    senators = get_senators()
+    legislatures = {
+        # '16': get_senators(16),
+        # '17': get_senators(17),
+        '18': get_senators(18)
+    }
     amendments_rows = get_amendments_from_csv()
     not_found = set()
 
     for row in amendments_rows:
-        sponsors_list = row['Firmatari'].split(', ')
-        all_matching_parties = []
-        for sponsor in sponsors_list:
-            try:
-                matching_parties = next(
-                    senator.parties for senator in senators if names_match(sponsor, senator.name))
-                all_matching_parties.extend(matching_parties)
-            except StopIteration as stop:
-                not_found.add(sponsor)
+        sponsors_list = row['FIRMATARI'].split(', ')
 
-        row['SPONSOR_PARTIES'] = ';'.join(all_matching_parties)
+        # legislature = row['Legislatura']
+
+        senators = legislatures.get('18')
+
+        found_parties = find_matching_parties(sponsors_list, senators)
+
+        row['SPONSOR_PARTIES'] = ';'.join(found_parties)
 
     write_new_rows(
         amendments_rows,
-        'test.csv'
+        './data/senato/with_parties.csv'
     )
 
     # print('not found {}:'.format(len(not_found)))
