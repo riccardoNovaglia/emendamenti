5ae9b24f5f3e0a7a7edd7fb9c4a9adb594af95f6
roberta <damianiroberta@hotmail.com>
2019-07-21 10:58:15 +0100

    Mark whether committee members along with contributions count.

--- countContributions.py
+++ countContributions.py
@@ -6,12 +6,19 @@ class Senator:
         self.name = name
         self.parties = parties
         self.contributions = 0
+        self.is_committee_member = False
 
     def __str__(self):
         return '{}, {}, {}'.format(self.name, self.parties, self.contributions)
 
     def increase_contribution(self):
         self.contributions = self.contributions + 1
+
+    def set_committee_member(self):
+        self.is_committee_member = True
+
+    def get_committee_member(self):
+        return 'yes' if self.is_committee_member else 'no'
 
 
 def get_amendments_from_csv():
@@ -28,9 +35,9 @@ def get_senators(legislature_number):
 
 def write_to_csv(senators, filename):
     print("Writing senators to file {}".format(filename))
-    data = [['NAME', 'PARTIES', 'CONTRIBUTIONS']]
+    data = [['NAME', 'PARTIES', 'CONTRIBUTIONS', 'COMMITTEE_MEMBER']]
     for senator in senators:
-        data.append([senator.name, ';'.join(senator.parties), senator.contributions])
+        data.append([senator.name, ';'.join(senator.parties), senator.contributions, senator.get_committee_member()])
 
     with open(filename, 'w+') as myFile:
         writer = csv.writer(myFile, lineterminator='\n')
@@ -47,8 +54,23 @@ def sponsor_name_in_sponsor_list(senator_name, sponsor_list):
     return False
 
 
+class CommitteeMember:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+def get_committee_members(filename):
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        return [CommitteeMember(row['NAME']) for row in reader]
+
+
 if __name__ == '__main__':
     senators = get_senators(13)
+    committee_members = get_committee_members('./data/senato/committee_members_senate_legislature_13.csv')
     amendments_rows = get_amendments_from_csv()
     not_found = set()
 
@@ -57,6 +79,10 @@ if __name__ == '__main__':
             amendment_sponsors = amendment['sponsor'].split(', ')
             if sponsor_name_in_sponsor_list(senator.name, amendment_sponsors):
                 senator.increase_contribution()
+
+        for committee_member_name in [member.name for member in committee_members]:
+            if senator.name == committee_member_name:
+                senator.set_committee_member()
 
     write_to_csv(
         senators,
